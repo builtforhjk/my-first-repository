782a495cbd34c78729f562549f7ba62aa53ced70
builtforhjk <31763723+builtforhjk@users.noreply.github.com>
2018-02-24 15:56:51 +0800

    Update pat-1073-Scientific-Notation.cpp

--- pat-1073-Scientific-Notation.cpp
+++ pat-1073-Scientific-Notation.cpp
@@ -1,47 +1,32 @@
 #include <iostream>
 #include <cstdio>
-#include <map>
 #include <cstring>
 using namespace std;
-string lowerCase(string str){
-  string tmp = str;
-  for(auto &ch : tmp){
-    if(ch>='A' && ch<='Z') ch = ch + ('a'-'A');
-  }
-  return tmp;
-}
-bool judge(char ch){
-  return (ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')||(ch>='0'&&ch<='9');
-}
 int main(){
   ios::sync_with_stdio(false);
-  map<string, int>stat;
-  string speech;
-  getline(cin,speech);
-  int start = 0, end = -1;
-  bool terminated = false;
-  while(1){
-    for(start = end+1;; ++start){
-      if(start >= speech.length()){
-        terminated = true;
-        break;
-      }
-      else if(judge(speech[start])) break;
+  string sc_number;
+  cin>>sc_number;
+  auto pivot = sc_number.find_first_of('E');
+  int exponent = stoi(sc_number.substr(pivot+1));
+  if(sc_number[0] == '-') cout<<"-";
+  string fraction = sc_number.substr(1, pivot-1);
+  if(exponent < 0){
+    cout<<"0.";
+    for(int i = -1; i > exponent; --i) cout<<"0";
+    cout<<fraction[0]<<fraction.substr(2,pivot-3)<<endl;
+  }
+  else if(exponent > 0){
+    int diff = exponent - ((int)fraction.length()-2);
+    if(diff > 0){
+      cout<<fraction[0]<<fraction.substr(2,pivot-3);
+      for(int i = 0; i < diff; ++i) cout<<"0";
     }
-    if(terminated) break;
-    end = start;
-    for(; judge(speech[end]); ++end);
-    ++stat[lowerCase(speech.substr(start, end-start))];
-  }
-  int pivot = 0;
-  for(auto &item : stat){
-    if(item.second > pivot) pivot = item.second;
-  }
-  for(auto item : stat){
-    if(item.second == pivot) {
-      cout<<item.first<<" "<<item.second<<endl;
-      break;
+    else if(diff < 0){
+      cout<<fraction[0]<<fraction.substr(2,exponent)<<"."<<fraction.substr(exponent+2);
     }
+    else cout<<fraction[0]<<fraction.substr(2,pivot-3);
+    cout<<endl;
   }
+  else cout<<fraction<<endl;
   return 0;
 }
